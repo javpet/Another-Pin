3dd44c44408487295db417be11e131c276f966eb
javpet <javorkai.peter@gmail.com>
2018-03-15 14:50:28 +0100

    Added price formatting with number_to_currency helper

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,23 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
   end
+
+  def create
+    @order = Order.new(form_params)
+
+    if @order.save
+      # We redirect the customer to the thanks page
+      redirect_to order_path(@order)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def form_params
+    params.require(:order).permit(:first_name, :last_name, :email, :address_1, :address_2, :city, :country, :postal_code)
+  end
 end
